9fef33ee1ddd70aec2fe3fa51ea8e73b582d1551
1313ou <1313ou@gmail.com>
2022-03-02 18:10:43 +0100

    wip

--- fn/src/main/resources/fn/sqltemplates/mysql/cleanup/sentences-cleanup.sql
+++ fn/src/main/resources/fn/sqltemplates/mysql/cleanup/sentences-cleanup.sql
@@ -3,8 +3,19 @@
 --     LEFT JOIN ${documents.table} AS pk ON fk.${sentences.documentid} = pk.${documents.documentid}
 -- WHERE fk.${sentences.documentid} IS NOT NULL AND pk.${documents.documentid} IS NULL;
 
+-- SELECT *
+--     FROM ${sentences.table} AS fk
+--     LEFT JOIN ${corpuses.table} AS pk ON fk.${sentences.corpusid} = pk.${corpuses.corpusid}
+-- WHERE fk.${sentences.corpusid} IS NOT NULL AND pk.${corpuses.corpusid} IS NULL;
+
 UPDATE
     ${sentences.table} AS fk
     LEFT JOIN ${documents.table} AS pk ON fk.${sentences.documentid} = pk.${documents.documentid}
 SET fk.${sentences.documentid} = NULL
 WHERE fk.${sentences.documentid} IS NOT NULL AND pk.${documents.documentid} IS NULL;
+
+UPDATE
+    ${sentences.table} AS fk
+    LEFT JOIN ${corpuses.table} AS pk ON fk.${sentences.corpusid} = pk.${corpuses.corpusid}
+    SET fk.${sentences.corpusid} = NULL
+WHERE fk.${sentences.corpusid} IS NOT NULL AND pk.${corpuses.corpusid} IS NULL;
--- fn/src/main/resources/fn/sqltemplates/sqlite/cleanup/documents-cleanup.sql
+++
@@ -1,1 +0,0 @@
-UPDATE ${sentences.table} AS fk LEFT JOIN ${documents.table} AS pk ON fk.${sentences.documentid} = pk.${documents.documentid}SET fk.${sentences.documentid} = NULLWHERE fk.${sentences.documentid} IS NOT NULL AND pk.${documents.documentid} IS NULL;
