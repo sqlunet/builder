3caa17c5a7649ecb7d2c7be55fc0997115bfb339
1313ou <1313ou@gmail.com>
2022-03-05 14:03:26 +0100

    wip

--- sl/src/main/resources/sl/sqltemplates/mysql/index/pbrolesets_vnclasses-index.sql
+++ sl/src/main/resources/sl/sqltemplates/mysql/index/pbrolesets_vnclasses-index.sql
@@ -1,3 +1,3 @@
 -- ALTER TABLE ${pbrolesets_vnclasses.table} ADD CONSTRAINT `pk_@{pbrolesets_vnclasses.table}` PRIMARY KEY                      (${pbrolesets_vnclasses.pbrolesetid},${pbrolesets_vnclasses.vnclassid});
-ALTER TABLE ${pbrolesets_vnclasses.table} ADD KEY        `k_@{pbrolesets_vnclasses.table}_${pbrolesets_vnclasses.pbroleset}` (${pbrolesets_vnclasses.pbroleset});
-ALTER TABLE ${pbrolesets_vnclasses.table} ADD KEY        `k_@{pbrolesets_vnclasses.table}_${pbrolesets_vnclasses.vnclass}`   (${pbrolesets_vnclasses.vnclass});
+ALTER TABLE ${pbrolesets_vnclasses.table} ADD KEY        `k_@{pbrolesets_vnclasses.table}_@{pbrolesets_vnclasses.pbroleset}` (${pbrolesets_vnclasses.pbroleset});
+ALTER TABLE ${pbrolesets_vnclasses.table} ADD KEY        `k_@{pbrolesets_vnclasses.table}_@{pbrolesets_vnclasses.vnclass}`   (${pbrolesets_vnclasses.vnclass});
