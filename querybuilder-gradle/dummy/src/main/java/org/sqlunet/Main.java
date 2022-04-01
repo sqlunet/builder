153ae2e655cfef92ace20c70dd36b384d60d12cc
1313ou <1313ou@gmail.com>
2022-04-01 20:18:53 +0200

    wip

--- querybuilder-gradle/dummy/src/main/java/org/sqlunet/Main.java
+++ querybuilder-gradle/dummy/src/main/java/org/sqlunet/Main.java
@@ -1,7 +1,11 @@
 package org.sqlunet;
 
+import org.sqlunet.wn.V;
+
 public class Main
 {
+	private static final String TEST = "'" + V.MEMBERS + "'";
+
 	public static void main(String[] args)
 	{
 	}
