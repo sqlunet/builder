477b2b9e83f9a0ebcae67f7efb03b16e561f3fcf
1313ou <1313ou@gmail.com>
2022-06-25 07:41:48 +0200

    fix sources

--- semantikos/_upload-bitbucket.sh
+++ semantikos/_upload-bitbucket.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
 #origin="https://semantikos@bitbucket.org/semantikos/semantikos2.git"
+
+REPODIR='bitbucket'
+MAKE_REPO=true
 
 # C O L O R S
 
@@ -12,67 +14,40 @@ M='\u001b[35m'
 C='\u001b[36m'
 Z='\u001b[0m'
 
-# D I R S
-
-datadir="db"
-datadir="$(readlink -m ${datadir})"
-echo "datadir=${datadir}"
-
-bitbucketrepo="repo1"
-bitbucketdir="bitbucket/${bitbucketrepo}"
-bitbucketdir="$(readlink -m ${bitbucketdir})"
-echo "bitbucketdir=${bitbucketdir}"
-
-# F I L E S
-
-files_dist="distrib.hsize distrib.md5 distrib.size"
-files_db="sqlunet.db sqlunet.db.md5"
-files_dbzip="sqlunet.db.zip sqlunet.db.zip.md5"
-files_sqlzip="sqlunet.sql.zip sqlunet.sql.zip.md5"
-files="${files_dist} ${files_db} ${files_dbzip} ${files_sqlzip}"
-files="${files_dist} ${files_dbzip}"
-
-# M A I N
-
-echo -e "${Y}S E M A N T I K O S   D B  +  S Q L  U P L O A D   T O   B I T B U C K E T   G I T   R E P O${Z}"
-mkdir -p "${bitbucketdir}"
-for f in ${files}; do
-	c1=${G}
-	if [ ! -e "${datadir}/${f}" ];then
-		c1=${R}
+# R E P O   S E T U P
+for repo in repo1 repo2 repo3; do
+	echo -e "${Y}${repo}${Z}"
+	pushd "${REPODIR}/${repo}" > /dev/null
+	
+	origin=$(git config --get remote.origin.url)
+	echo -e "${M}${origin}${Z}"
+	
+	if [ ! -z "${MAKE_REPO}"]; then
+		
+		rm -fR .git
+		git init
+		git add *
+		git commit -m "Initial"
+		git remote add origin "${origin}"
+		
+		commit="$(git rev-parse HEAD)"
+		echo -e "${G}${commit}${Z}"
+	
 	fi
-	c2=${G}
-	if [ -e "${bitbucketdir}/${f}" ];then
-		c2=${R}
-	fi
-	echo -e "${B}${f}${Z} from ${c1}${datadir}${Z}  to ${c2}${bitbucketdir}${Z}"
+	
+	popd > /dev/null
 done
 
-read -p "Are you sure you want to copy to bitbucket repo '${bitbucketrepo}' ? " -n 1 -r
-echo    # (optional) move to a new line
-echo -e "${Z}"
-if ! [[ $REPLY =~ ^[Yy]$ ]]; then
-    exit 2
-fi
-echo 'Proceeding ...'
-echo -e "${Y}upload${Z}"
-for f in ${files}; do
-	echo -e "${B}${f}${Z}"
-	cp -P ${datadir}/${f} ${bitbucketdir}/
+# R E M O T E   R E P O
+for repo in repo1 repo2 repo3; do
+	echo -e "${C}${repo}${Z}"
+	pushd "${REPODIR}/${repo}" > /dev/null
+	
+	git push origin master
+	
+	commit="$(git rev-parse HEAD)"
+	echo -e "${G}${commit}${Z}"
+	
+	popd > /dev/null
 done
-
-read -p "Are you sure you want to upload to bitbucket? " -n 1 -r
-echo    # (optional) move to a new line
-echo -e "${Z}"
-if ! [[ $REPLY =~ ^[Yy]$ ]]; then
-    exit 2
-fi
-echo 'Proceeding ...'
-echo -e "${Y}upload${Z}"
-pushd ${bitbucketdir} > /dev/null
-git status
-git add .
-git commit -m 'Rebuild'
-git push origin master
-popd > /dev/null
 
--- semantikos/sql/sources.sql
+++ semantikos/sql/sources.sql
@@ -9,27 +9,30 @@ CREATE TABLE IF NOT EXISTS sources (
   reference TEXT DEFAULT NULL,
   PRIMARY KEY (idsource)
 );
-INSERT INTO `sources` (idsource,name,version,wnversion,url,provider,reference) VALUES (1,'WordNet','3.1','3.1','http://wordnet.princeton.edu/','Princeton University','George A. Miller (1995). WordNet: A Lexical Database for English. Communications of the ACM Vol. 38, No. 11: 39-41.
+INSERT INTO `sources` (idsource,name,version,wnversion,url,provider,reference) VALUES 
+ (1,'WordNet','3.1','3.1','http://wordnet.princeton.edu/','Princeton University','George A. Miller (1995). WordNet: A Lexical Database for English. Communications of the ACM Vol. 38, No. 11: 39-41.
 Christiane Fellbaum (1998, ed.) WordNet: An Electronic Lexical Database. Cambridge, MA: MIT Press.'),
- (2,'VerbNet','3.2','3.0','http://verbs.colorado.edu/~mpalmer/projects/verbnet.html','University of Colorado','Kain Kipper, Anna Korhonen, Neville Ryant, Martha Palmer, A Large-scale Classification of English Verbs, Language Resources and Evaluation Journal, 42(1), pp. 21-40, Springer Netherland, 2008.
+ (2,'Open English Wordnet','3.x','3.x','https://github.com/globalwordnet/english-wordnet','Global WordNet Association','John P. McCrae, Alexandre Rademaker, Francis Bond, Ewa Rudnicka, and Christiane Fellbaum, English WordNet 2019 – An Open-Source WordNet for English, Proceedings of the 10th Global Wordnet Conference, pp. 245–252, Wroclaw, Poland. Global Wordnet Association, 2019'),
+ (10,'VerbNet','3.2','3.0','http://verbs.colorado.edu/~mpalmer/projects/verbnet.html','University of Colorado','Kain Kipper, Anna Korhonen, Neville Ryant, Martha Palmer, A Large-scale Classification of English Verbs, Language Resources and Evaluation Journal, 42(1), pp. 21-40, Springer Netherland, 2008.
 Karin Kipper Schuler, Anna Korhonen, Susan W. Brown, VerbNet overview, extensions, mappings and apps, Tutorial, NAACL-HLT 2009, Boulder, Colorado'),
- (3,'PropBank','3.1',NULL,'http://verbs.colorado.edu/~mpalmer/projects/ace.html','University of Colorado','Paul Kingsbury and Martha Palmer.From Treebank to PropBank. 2002. In Proceedings of the 3rd International Conference on Language Resources and Evaluation (LREC-2002), Las Palmas, Spain.
+ (11,'PropBank','3.1',NULL,'http://verbs.colorado.edu/~mpalmer/projects/ace.html','University of Colorado','Paul Kingsbury and Martha Palmer. From Treebank to PropBank. 2002. In Proceedings of the 3rd International Conference on Language Resources and Evaluation (LREC-2002), Las Palmas, Spain.
 Martha Palmer, Dan Gildea, Paul Kingsbury, The Proposition Bank: A Corpus Annotated with Semantic Roles Computational Linguistics Journal, 31:1, 2005.
 The Necessity of Parsing for Predicate Argument Recognition. Daniel Gildea and Martha Palmer. 2002. In Proceedings of ACL 2002, Philadelphia, PA.'),
- (4,'SemLink','1.2.2c',NULL,'https://verbs.colorado.edu/semlink/','University of Colorado','Martha Palmer. 2009. Semlink: Linking PropBank, VerbNet and FrameNet. Proceedings of the Generative Lexicon Conference Pisa, Italy.'),
- (5,'FrameNet','1.7',NULL,'https://framenet.icsi.berkeley.edu/fndrupal/','University of Berkeley','Collin F. Baker and Charles J. Fillmore and John B.Lowe. The Berkeley FrameNet Project. International Computer Science Institute. ACL ''98 Proceedings of the 36th Annual Meeting of the Association for Computational Linguistics and 17th International Conference on Computational Linguistics - Volume 1 archive Pages 86-90'),
- (6,'PredicateMatrix','1.3',NULL,'http://adimen.si.ehu.es/web/PredicateMatrix/','University of the Basque Country','López de Lacalle M., Laparra E. and Rigau G. First steps towards a Predicate Matrix. Proceedings of the 7th Global WordNet Conference (GWC 2014). Tartu, Estonia. 2014.
+ (12,'SemLink','1.2.2c',NULL,'https://verbs.colorado.edu/semlink/','University of Colorado','Martha Palmer. 2009. Semlink: Linking PropBank, VerbNet and FrameNet. Proceedings of the Generative Lexicon Conference Pisa, Italy.'),
+ (20,'FrameNet','1.7',NULL,'https://framenet.icsi.berkeley.edu/fndrupal/','University of Berkeley','Collin F. Baker and Charles J. Fillmore and John B.Lowe. The Berkeley FrameNet Project. International Computer Science Institute. ACL ''98 Proceedings of the 36th Annual Meeting of the Association for Computational Linguistics and 17th International Conference on Computational Linguistics - Volume 1 archive Pages 86-90'),
+ (30,'PredicateMatrix','1.3',NULL,'http://adimen.si.ehu.es/web/PredicateMatrix/','University of the Basque Country','López de Lacalle M., Laparra E. and Rigau G. First steps towards a Predicate Matrix. Proceedings of the 7th Global WordNet Conference (GWC 2014). Tartu, Estonia. 2014.
 López de Lacalle M., Laparra E. and Rigau G. Predicate Matrix: extending SemLink through WordNet mappings. Proceedings of the 9th international conference on Language Resources and Evaluation (LREC 2014). Reykjavik, Iceland. 2014.
 López de Lacalle M., Laparra E., Aldabe I. and Rigau G. Predicate Matrix. Automatically extending the semantic interoperability between predicate resources. Language Resources and Evaluation. Forthcoming.
 López de Lacalle M., Laparra E., Aldabe I. and Rigau G. A Multilingual Predicate Matrix. Proceedings of the 10th international conference on Language Resources and Evaluation (LREC 2016). Portoroẑ, Slovenia. 2016.'),
- (7,'BNC','2001','any','http://ucrel.lancs.ac.uk/bncfreq/',NULL,NULL),
- (8,'SyntagNet','1.0','3.0','http://syntagnet.org/','The Sapienza NLP Group, Sapienza University of Rome.','Marco Maru, Federico Scozzafava, Federico Martelli and Roberto Navigli. SyntagNet: Challenging Supervised Word Sense Disambiguation with Lexical-Semantic Combinations. Proc. of the 2019 Conference on Empirical Methods in Natural Language Processing (EMNLP 2019), Hong Kong, China, November 3-7, 2019.'),
- (10,'SUMO','cvs','3.0','http://www.ontologyportal.org/','SUMO','Niles, I., & Pease, A., (2001), Toward a Standard Upper Ontology, in Proceedings of the 2nd International Conference on Formal Ontology in Information Systems (FOIS-2001), Chris Welty and Barry Smith, eds, pp2-9. '),
- (11,'GlossLF','3.0','3.0','http://wordnet.princeton.edu/wordnet/download/standoff/','Princeton University',NULL),
- (12,'ILFWn','0.2','3.0','http://nlp.uned.es/semantics/ilf/ilf.html','Universidad Politécnica de Madrid',NULL),
- (20,'Sense Maps 20-21',NULL,'2.0-2.1','http://wordnet.princeton.edu/','Princeton University',NULL),
- (21,'Sense Maps 21-30',NULL,'2.1-3.0','http://wordnet.princeton.edu/','Princeton University',NULL),
- (22,'Synset Maps 1-8-3.0',NULL,'1-8 ... 3.0','http://www.talp.upc.edu/index.php/technology/tools/45-textual-pr','TALP - Universitat Politècnica de Catalunya','Jordi Daudé and Lluís Padró and German Rigau. Mapping WordNets Using Structural Information. 38th Annual Meeting of the Association for Computational Linguistics (ACL''2000)., Hong Kong. 2000.
+ (40,'BNC','2001','any','http://ucrel.lancs.ac.uk/bncfreq/',NULL,NULL),
+ 
+ (41,'SyntagNet','1.0','3.0','http://syntagnet.org/','The Sapienza NLP Group, Sapienza University of Rome.','Marco Maru, Federico Scozzafava, Federico Martelli and Roberto Navigli. SyntagNet: Challenging Supervised Word Sense Disambiguation with Lexical-Semantic Combinations. Proc. of the 2019 Conference on Empirical Methods in Natural Language Processing (EMNLP 2019), Hong Kong, China, November 3-7, 2019.'),
+ (50,'SUMO','cvs','3.0','http://www.ontologyportal.org/','SUMO','Niles, I., & Pease, A., (2001), Toward a Standard Upper Ontology, in Proceedings of the 2nd International Conference on Formal Ontology in Information Systems (FOIS-2001), Chris Welty and Barry Smith, eds, pp2-9. '),
+ (51,'GlossLF','3.0','3.0','http://wordnet.princeton.edu/wordnet/download/standoff/','Princeton University',NULL),
+ (52,'ILFWn','0.2','3.0','http://nlp.uned.es/semantics/ilf/ilf.html','Universidad Politécnica de Madrid',NULL),
+ (60,'Sense Maps 20-21',NULL,'2.0-2.1','http://wordnet.princeton.edu/','Princeton University',NULL),
+ (61,'Sense Maps 21-30',NULL,'2.1-3.0','http://wordnet.princeton.edu/','Princeton University',NULL),
+ (62,'Synset Maps 1-8-3.0',NULL,'1-8 ... 3.0','http://www.talp.upc.edu/index.php/technology/tools/45-textual-pr','TALP - Universitat Politècnica de Catalunya','Jordi Daudé and Lluís Padró and German Rigau. Mapping WordNets Using Structural Information. 38th Annual Meeting of the Association for Computational Linguistics (ACL''2000)., Hong Kong. 2000.
 Jordi Daudé and Lluís Padró and German Rigau. A Complete WN1.5 to WN1.6 Mapping. NAACL Workshop ''WordNet and Other Lexical Resources: Applications, Extensions and Customizations'' (NAACL''2001)., Pittsburg, PA, USA. 2001.
 Jordi Daudé and Lluís Padró and German Rigau. Making Wordnet Mappings Robust. Proceedings of the 19th Congreso de la Sociedad Española para el Procesamiento del Lenguage Natural, SEPLN, Universidad Universidad de Alcalá de Henares. Madrid, Spain. 2003.
 Jordi Daudé and Lluís Padró and German Rigau. Validation and Tuning of Wordnet Mapping Techniques. Proceedings of the International Conference on Recent Advances in Natural Language Processing (RANLP''03), Borovets, Bulgaria. 2003.
@@ -37,7 +40,6 @@ Jordi Daudé. Enlace de Jerarquías Usando el Etiquetado por Relajación. PhD. T
 Jordi Daudé and Lluís Padró and German Rigau. Experiments on Applying Relaxation Labeling to Map Multilingual Hierarchies. Technical Report LSI-99-5-R. Departament de LSI. Universitat PolitÚcnica de Catalunya. 1999.
 Jordi Daudé and Lluís Padró and German Rigau. Mapping Multilingual Hierarchies Using Relaxation Labeling. Joint SIGDAT Conference on Empirical Methods in Natural Language Processing and Very Large Corpora (EMNLP/VLC''99)., Maryland, US. 1999.
 Jordi Daudé and Lluís Padró and German Rigau. Proposals on Mapping Multilingual Hierarchies. Dept. LSI - Universitat PolitÚcnica de Catalunya. 1999. Technical Report LSI-99-7-R.'),
- (23,'Synset Maps 3.0-3.1',NULL,'3.0-3.1','http://www.hyperdic.net/en/doc/mapping','Hyperdictionary',NULL),
- (30,'Open English WordNet','3.x','3.x','https://github.com/globalwordnet/english-wordnet','Global WordNet Association',NULL);
+ (63,'Synset Maps 3.0-3.1',NULL,'3.0-3.1','http://www.hyperdic.net/en/doc/mapping','Hyperdictionary',NULL);
 
-DELETE FROM sources WHERE idsource >=10;
+DELETE FROM sources WHERE idsource >=50;
