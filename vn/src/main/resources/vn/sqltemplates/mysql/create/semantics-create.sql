d22e404bffcc8b443b0a2b2951cb9992b13b356f
1313ou <1313ou@gmail.com>
2022-08-28 09:35:12 +0200

    collation utf8mb4_0900_as_ci

--- vn/src/main/resources/vn/sqltemplates/mysql/create/semantics-create.sql
+++ vn/src/main/resources/vn/sqltemplates/mysql/create/semantics-create.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS ${semantics.table} (
     ${semantics.semanticsid} INTEGER NOT NULL,
-    ${semantics.semantics} TEXT CHARACTER SET utf8 COLLATE utf8mb4_0900_as_cs NOT NULL
+    ${semantics.semantics} TEXT CHARACTER SET utf8 COLLATE utf8mb4_0900_as_ci NOT NULL
 );
--- vn/src/main/resources/vn/sqltemplates/mysql/create/syntaxes-create.sql
+++ vn/src/main/resources/vn/sqltemplates/mysql/create/syntaxes-create.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS ${syntaxes.table} (
     ${syntaxes.syntaxid} INTEGER NOT NULL,
-    ${syntaxes.syntax} TEXT NOT NULL
+    ${syntaxes.syntax} TEXT CHARACTER SET utf8 COLLATE utf8mb4_0900_as_ci NOT NULL
 );
