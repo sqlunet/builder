fbd2b7fb74537db9cb7c78f3fc0d1cfae1c86d78
1313ou <1313ou@gmail.com>
2022-08-28 09:35:41 +0200

    fk fixes

--- pm/src/main/resources/pm/sqltemplates/mysql/reference/pms-reference.sql
+++ pm/src/main/resources/pm/sqltemplates/mysql/reference/pms-reference.sql
@@ -1,16 +1,16 @@
--- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.wordid}'      FOREIGN KEY (${pms.wordid})         REFERENCES ${wnwords.table}    (${wnwords.wordid});
--- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.synsetid}'    FOREIGN KEY (${pms.synsetid})       REFERENCES ${wnsynsets.table}  (${wnsynsets.synsetid});
+-- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.wordid}`      FOREIGN KEY (${pms.wordid})         REFERENCES ${wnwords.table}    (${wnwords.wordid});
+-- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.synsetid}`    FOREIGN KEY (${pms.synsetid})       REFERENCES ${wnsynsets.table}  (${wnsynsets.synsetid});
 
--- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.vnclassid}'   FOREIGN KEY (${pms.vnclassid})      REFERENCES ${vnclasses.table}  (${vnclasses.classid});
--- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.vnroleid}'    FOREIGN KEY (${pms.vnroleid})       REFERENCES ${vnroles.table}    (${vnroles.roleid});
--- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.vnwordid}'    FOREIGN KEY (${pms.vnwordid}        REFERENCES ${vnwords.table}    (${vnwords.vnwordid});
+-- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.vnclassid}`   FOREIGN KEY (${pms.vnclassid})      REFERENCES ${vnclasses.table}  (${vnclasses.classid});
+-- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.vnroleid}`    FOREIGN KEY (${pms.vnroleid})       REFERENCES ${vnroles.table}    (${vnroles.roleid});
+-- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.vnwordid}`    FOREIGN KEY (${pms.vnwordid})       REFERENCES ${vnwords.table}    (${vnwords.vnwordid});
 
--- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.pbrolesetid}' FOREIGN KEY (${pms.pbrolesetid})    REFERENCES ${pbrolesets.table} (${pbrolesets.rolesetid});
--- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.pbrolesetid}' FOREIGN KEY (${pms.pbrolesetid})    REFERENCES ${pbroles.table}    (${pbroles.roleid});
--- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.pbwordid}'    FOREIGN KEY (${pms.pbwordid})       REFERENCES ${pbwords.table}    (${pbwords.pbwordid});
+-- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.pbrolesetid}` FOREIGN KEY (${pms.pbrolesetid})    REFERENCES ${pbrolesets.table} (${pbrolesets.rolesetid});
+-- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.pbroleid}`    FOREIGN KEY (${pms.pbroleid})       REFERENCES ${pbroles.table}    (${pbroles.roleid});
+-- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.pbwordid}`    FOREIGN KEY (${pms.pbwordid})       REFERENCES ${pbwords.table}    (${pbwords.pbwordid});
 
--- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.fnframeid}'   FOREIGN KEY (${pms.fnframeid})      REFERENCES ${fnframes.table}   (${fnframes.frameid});
--- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.fnfeid}'      FOREIGN KEY (${pms.fnfeid})         REFERENCES ${fnfes.table}      (${fnfes.feid});
--- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.fnfetypeid}'  FOREIGN KEY (${pms.fnfetypeid})     REFERENCES ${fnfetypes.table}  (${fnfetypes.fetypeid});
--- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.fnluid}'      FOREIGN KEY (${pms.fnluid})         REFERENCES ${fnlexunits.table} (${fnlexunits.luid});
--- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.fnwordid}'    FOREIGN KEY (${pms.fnwordid})       REFERENCES ${fnwords.table}    (${fnwords.fnwordid});
+-- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.fnframeid}`   FOREIGN KEY (${pms.fnframeid})      REFERENCES ${fnframes.table}   (${fnframes.frameid});
+-- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.fnfeid}`      FOREIGN KEY (${pms.fnfeid})         REFERENCES ${fnfes.table}      (${fnfes.feid});
+-- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.fnfetypeid}`  FOREIGN KEY (${pms.fnfetypeid})     REFERENCES ${fnfetypes.table}  (${fnfetypes.fetypeid});
+-- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.fnluid}`      FOREIGN KEY (${pms.fnluid})         REFERENCES ${fnlexunits.table} (${fnlexunits.luid});
+-- ALTER TABLE ${pms.table} ADD CONSTRAINT `fk_@{pms.table}_@{pms.fnwordid}`    FOREIGN KEY (${pms.fnwordid})       REFERENCES ${fnwords.table}    (${fnwords.fnwordid});
--- vn/src/main/resources/vn/sqltemplates/mysql/reference/members_senses-reference.sql
+++ vn/src/main/resources/vn/sqltemplates/mysql/reference/members_senses-reference.sql
@@ -1,2 +1,4 @@
 ALTER TABLE ${members_senses.table} ADD CONSTRAINT `fk_@{members_senses.table}_@{members_senses.classid}` FOREIGN KEY (${members_senses.classid}) REFERENCES ${classes.table} (${classes.classid});
 ALTER TABLE ${members_senses.table} ADD CONSTRAINT `fk_@{members_senses.table}_@{members_senses.vnwordid}` FOREIGN KEY (${members_senses.vnwordid}) REFERENCES ${words.table} (${words.vnwordid});
+-- ALTER TABLE ${members_senses.table} ADD CONSTRAINT `fk_@{members_senses.table}_@{members_senses.synsetid}` FOREIGN KEY (${members_senses.synsetid}) REFERENCES ${wnsynsets.table} (${wnsynsets.synsetid});
+-- ALTER TABLE ${members_senses.table} ADD CONSTRAINT `fk_@{members_senses.table}_@{members_senses.wordid}` FOREIGN KEY (${members_senses.wordid}) REFERENCES ${wnwords.table} (${wnwords.wordid});
