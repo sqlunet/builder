c2a539774862ffd7bc8a7fa0e423ffdd1bbd6493
1313ou <1313ou@gmail.com>
2022-08-31 15:56:01 +0200

    fn_patterns view

--- fn/src/main/resources/fn/sqltemplates/mysql/create/ferealizations_valenceunits-create.sql
+++ fn/src/main/resources/fn/sqltemplates/mysql/create/ferealizations_valenceunits-create.sql
@@ -2,3 +2,4 @@ CREATE TABLE IF NOT EXISTS ${ferealizations_valenceunits.table} (
         ${ferealizations_valenceunits.ferid} INTEGER NOT NULL,
         ${ferealizations_valenceunits.vuid} INTEGER NOT NULL
 );
+-- CREATE VIEW  ${ferealizations_valenceunits.alias} AS SELECT * FROM ${ferealizations_valenceunits.table};
