a85ad961843c0a9b92b7a04aab30ac4e16f00494
1313ou <1313ou@gmail.com>
2022-09-09 09:23:09 +0200

    fixes tests

--- fn/src/test/java/org/sqlbuilder/fn/TestCollector.java
+++ fn/src/test/java/org/sqlbuilder/fn/TestCollector.java
@@ -34,10 +34,31 @@ public class TestCollector
 			System.out.println(C.get("four"));
 		}
 		System.err.println("[AFTER]" + C.status());
+		try { System.out.println(C.get("one")); } catch (IllegalStateException ise) {System.out.println(ise.getMessage());}
+		try { System.out.println(C.get("two")); } catch (IllegalStateException ise) {System.out.println(ise.getMessage());}
+		try { System.out.println(C.get("three")); } catch (IllegalStateException ise) {System.out.println(ise.getMessage());}
+		try { System.out.println(C.get("four")); } catch (IllegalStateException ise) {System.out.println(ise.getMessage());}
+	}
+
+	@Test(expected=IllegalStateException.class)
+	public void testFail()
+	{
+		for (var e : SET)
+		{
+			C.add(e);
+		}
+
+		System.err.println("[BEFORE]" + C.status());
+		try (var ignored = C.open())
+		{
+			System.err.println("[ACTIVE]" + C.status());
+			System.out.println(C.get("one"));
+			System.out.println(C.get("two"));
+			System.out.println(C.get("three"));
+			System.out.println(C.get("four"));
+		}
+		System.err.println("[AFTER]" + C.status());
 		System.out.println(C.get("one"));
-		System.out.println(C.get("two"));
-		System.out.println(C.get("three"));
-		System.out.println(C.get("four"));
 	}
 
 	@Test
@@ -63,13 +84,39 @@ public class TestCollector
 			System.out.println("d " + D.get("four"));
 		}
 		System.err.println("[AFTER]" + C.status());
+		try { System.out.println("c " + C.get("one")); } catch (IllegalStateException ise) {System.out.println(ise.getMessage());}
+		try { System.out.println("c " + C.get("two")); } catch (IllegalStateException ise) {System.out.println(ise.getMessage());}
+		try { System.out.println("c " + C.get("three")); } catch (IllegalStateException ise) {System.out.println(ise.getMessage());}
+		try { System.out.println("c " + C.get("four")); } catch (IllegalStateException ise) {System.out.println(ise.getMessage());}
+		try { System.out.println("d " + D.get("one")); } catch (IllegalStateException ise) {System.out.println(ise.getMessage());}
+		try { System.out.println("d " + D.get("two")); } catch (IllegalStateException ise) {System.out.println(ise.getMessage());}
+		try { System.out.println("d " + D.get("three")); } catch (IllegalStateException ise) {System.out.println(ise.getMessage());}
+		try { System.out.println("d " + D.get("four")); } catch (IllegalStateException ise) {System.out.println(ise.getMessage());}
+	}
+
+	@Test(expected=IllegalStateException.class)
+	public void test2Fail()
+	{
+		for (var e : SET)
+		{
+			C.add(e);
+			D.add(e);
+		}
+
+		System.err.println("[BEFORE]" + C.status());
+		try (var ignored = C.open(); var ignored2 = D.open())
+		{
+			System.err.println("[ACTIVE]" + C.status() + D.status());
+			System.out.println("c " + C.get("one"));
+			System.out.println("c " + C.get("two"));
+			System.out.println("c " + C.get("three"));
+			System.out.println("c " + C.get("four"));
+			System.out.println("d " + D.get("one"));
+			System.out.println("d " + D.get("two"));
+			System.out.println("d " + D.get("three"));
+			System.out.println("d " + D.get("four"));
+		}
+		System.err.println("[AFTER]" + C.status());
 		System.out.println("c " + C.get("one"));
-		System.out.println("c " + C.get("two"));
-		System.out.println("c " + C.get("three"));
-		System.out.println("c " + C.get("four"));
-		System.out.println("d " + D.get("one"));
-		System.out.println("d " + D.get("two"));
-		System.out.println("d " + D.get("three"));
-		System.out.println("d " + D.get("four"));
 	}
 }
