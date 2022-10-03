6b46427dc781e930f1a189de1d1d04fe0fde356f
1313ou <1313ou@gmail.com>
2022-10-03 21:15:25 +0200

    cosmetic

--- bnc/src/main/resources/bnc/sqltemplates/sqlite/create/bncs-create.sql
+++ bnc/src/main/resources/bnc/sqltemplates/sqlite/create/bncs-create.sql
@@ -5,5 +5,4 @@ ${bncs.posid} CHARACTER (1) CHECK( ${bncs.posid} IN ('n','v','a','r','s') ) NOT 
 ${bncs.freq} INTEGER DEFAULT NULL,
 ${bncs.range} INTEGER DEFAULT NULL,
 ${bncs.disp} FLOAT DEFAULT NULL
-)
-;
+);
