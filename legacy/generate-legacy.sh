46fcb9518269b292a8870cf5da9c476817743e5e
1313ou <1313ou@gmail.com>
2022-10-05 11:04:50 +0200

    legacy

---
+++ legacy/generate-legacy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source define_colors.sh
+
+function generate()
+{
+	local what=$1
+	local from=$2
+	local to=$3
+	java -cp generate-legacy.jar org.sqlbuilder2.legacy.LegacyModule legacy.properties $what from=$from to=$to
+}
+
+generate sensekeys 30
+generate synsets 30 XX
+
