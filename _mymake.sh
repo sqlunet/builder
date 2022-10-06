29100407e626aae668af9475c28b1beb2391c317
1313ou <1313ou@gmail.com>
2022-10-06 20:41:20 +0200

    script enhancements

--- _mymake.sh
+++ _mymake.sh
@@ -10,6 +10,11 @@ M='\u001b[35m'
 C='\u001b[36m'
 Z='\u001b[0m'
 
+echo -e "${Y}L E G A C Y${Z}"
+pushd legacy > /dev/null
+./generate-legacy.sh all
+popd  > /dev/null
+
 echo -e "${Y}S C H E M A${Z}"
 ./generate-schema.sh all
 
