6f32dd113a017488844a80295c9e77f550aab9e7
1313ou <1313ou@gmail.com>
2023-01-10 20:38:32 +0100

    copy to bitbucket repos

--- vn/src/main/resources/vn/sqltemplates/sqlite/create/examples-create.sql
+++ vn/src/main/resources/vn/sqltemplates/sqlite/create/examples-create.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS ${examples.table} (
 ${examples.exampleid} INTEGER NOT NULL,
-${examples.example} TEXT COLLATE utf8mb4_0900_as_ci NOT NULL
+${examples.example} TEXT NOT NULL
 );
--- vn/src/main/resources/vn/sqltemplates/sqlite/create/syntaxes-create.sql
+++ vn/src/main/resources/vn/sqltemplates/sqlite/create/syntaxes-create.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS ${syntaxes.table} (
 ${syntaxes.syntaxid} INTEGER NOT NULL,
-${syntaxes.syntax} TEXT COLLATE utf8mb4_0900_as_ci NOT NULL
+${syntaxes.syntax} TEXT NOT NULL
 );
