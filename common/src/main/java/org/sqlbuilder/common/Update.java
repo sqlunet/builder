72b5c7815371da5b2c5d2a66f4d7b6b45713b9d0
1313ou <1313ou@gmail.com>
2024-11-15 08:04:25 +0100

    reformat

--- common/src/main/java/org/sqlbuilder/common/Update.java
+++ common/src/main/java/org/sqlbuilder/common/Update.java
@@ -9,16 +9,20 @@ import java.util.function.Function;
 
 public class Update
 {
-	public static <T extends Resolvable<U, R>, U, R> void update(final Collection<T> collection, final File file, final String header, final String table, //
-			final Function<U, R> resolver, //
-			final Function<R, String> setStringifier, //
-			final Function<U, String> whereStringifier //
+	public static <T extends Resolvable<U, R>, U, R> void update(
+			final Collection<T> collection,
+			final File file,
+			final String header,
+			final String table,
+			final Function<U, R> resolver,
+			final Function<R, String> setStringifier,
+			final Function<U, String> whereStringifier
 	) throws FileNotFoundException
 	{
 		try (PrintStream ps = new PrintStream(new FileOutputStream(file)))
 		{
 			ps.println("-- " + header);
-			if (collection.size() > 0)
+			if (!collection.isEmpty())
 			{
 				collection.forEach(item -> {
 
