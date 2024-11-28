0a3eda27d7f88f7d42c4dca95667016492c10512
1313ou <1313ou@gmail.com>
2024-11-28 09:13:43 +0100

    to kotlin

--- common/src/main/java/org/sqlbuilder/common/Resolver2.kt
+++ common/src/main/java/org/sqlbuilder/common/Resolver2.kt
@@ -1,26 +1,12 @@
-package org.sqlbuilder.common;
+package org.sqlbuilder.common
 
-import java.util.Map;
-import java.util.function.BiFunction;
+import java.util.function.BiFunction
 
-public abstract class Resolver2<T, U, R> implements BiFunction<T, U, R>
-{
-	public final Map<T, Map<U, R>> map;
+abstract class Resolver2<T, U, R>(val map: Map<T, Map<U, R>>) : BiFunction<T, U, R> {
 
-	public Resolver2(final Map<T, Map<U, R>> map)
-	{
-		this.map = map;
-	}
-
-	@Nullable
-	@Override
-	public R apply(final T k, U k2)
-	{
-		var m2 = map.get(k);
-		if (m2 == null)
-		{
-			return null;
-		}
-		return m2.get(k2);
-	}
+    @Nullable
+    override fun apply(k: T, k2: U): R {
+        val m2 = map[k]!!
+        return m2[k2]!!
+    }
 }
