589700abf60158130fbdd06472ca075aab9ce00d
1313ou <1313ou@gmail.com>
2024-12-06 08:11:03 +0100

    main entry point

--- main/src/main/java/org/sqlbuilder/Main.kt
+++ main/src/main/java/org/sqlbuilder/Main.kt
@@ -3,6 +3,17 @@
  */
 package org.sqlbuilder
 
+import org.sqlbuilder.bnc.BncModule
+import org.sqlbuilder.fn.FnModule
+import org.sqlbuilder.pb.PbModule
+import org.sqlbuilder.pm.PmModule
+import org.sqlbuilder.sl.SlModule
+import org.sqlbuilder.sn.SnModule
+import org.sqlbuilder.su.SuModule
+import org.sqlbuilder.vn.VnModule
+import org.sqlbuilder2.legacy.LegacyModule
+import java.io.File
+
 /**
  * Main class
  *
@@ -12,13 +23,53 @@ class Main {
 
     companion object {
 
-        /**
-         * Main entry point
-         *
-         * @param args command-line arguments
-          */
+        val modules = listOf(
+            "bnc",
+            "sn",
+            "vn",
+            "pb",
+            "sl",
+            "fn",
+            "pm",
+            "su",
+            "leg",
+        )
+
+        fun run0(module: String, args: Array<String>) {
+            when (module) {
+                "bnc" -> BncModule.main(args)
+                "sn"  -> SnModule.main(args)
+                "vn"  -> VnModule.main(args)
+                "pb"  -> PbModule.main(args)
+                "sl"  -> SlModule.main(args)
+                "fn"  -> FnModule.main(args)
+                "pm"  -> PmModule.main(args)
+                "su"  -> SuModule.main(args)
+                "leg" -> LegacyModule.main(args)
+                else  -> {}
+            }
+        }
+
+        fun run(module: String, args: Array<String>) {
+            println("$module ${args.joinToString(separator = " ")}")
+        }
+
+        @JvmStatic
         fun main(args: Array<String>) {
-            // empty
+            val cwd = File(".").absolutePath
+            println("CWD: $cwd")
+            val module = args[0]
+            val ops = args.slice(1..args.size - 1)
+            val runModules = if ("all" == module) modules else listOf(module)
+            runModules.forEach { m ->
+                println("\nMOD: $m")
+                ops.forEach { o ->
+                    val args = if ("all" == o) arrayOf("-data", "-resolve", "-update", "-export") else arrayOf(o)
+                    args.forEach {
+                        run(m, if ("-data" == it) arrayOf("$m/$m.properties") else arrayOf(it, "$m/$m.properties"))
+                    }
+                }
+            }
         }
     }
 }
