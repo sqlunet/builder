50dc7fc40199bc20e5e3c205ffbe98d1cd5d7c60
1313ou <1313ou@gmail.com>
2024-12-07 17:57:34 +0100

    cleanup

--- common/src/main/java/org/sqlbuilder/common/Resolver2.kt
+++ common/src/main/java/org/sqlbuilder/common/Resolver2.kt
@@ -2,7 +2,6 @@ package org.sqlbuilder.common
 
 abstract class Resolver2<T, U, R>(val map: Map<T, Map<U, R>>) : (T, U) -> R? {
 
-    @Nullable
     override fun invoke(k: T, k2: U): R? {
         return map[k]?.get(k2)
     }
