f68620297d5311488c0de815532374627edf58a3
1313ou <1313ou@gmail.com>
2024-12-07 17:47:25 +0100

    fix nullability

--- common/src/main/java/org/sqlbuilder/common/Resolver2.kt
+++ common/src/main/java/org/sqlbuilder/common/Resolver2.kt
@@ -1,10 +1,9 @@
 package org.sqlbuilder.common
 
-abstract class Resolver2<T, U, R>(val map: Map<T, Map<U, R>>) : (T, U) -> R {
+abstract class Resolver2<T, U, R>(val map: Map<T, Map<U, R>>) : (T, U) -> R? {
 
     @Nullable
-    override fun invoke(k: T, k2: U): R {
-        val m2 = map[k]!!
-        return m2[k2]!!
+    override fun invoke(k: T, k2: U): R? {
+        return map[k]?.get(k2)
     }
 }
