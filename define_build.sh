a3c2cbf311ba3690e10cbed3cd6709a41a9e2110
1313ou <1313ou@gmail.com>
2025-06-22 09:03:34 +0200

    bump new version

--- define_build.sh
+++ define_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export TAG=2024
+export TAG=2025
 export TAG31=31
 export BUILDMAJOR=2
 export BUILDMINOR=3
-export BUILDREV=1
+export BUILDREV=2
 export BUILD="${BUILDMAJOR}.${BUILDMINOR}.${BUILDREV}"
