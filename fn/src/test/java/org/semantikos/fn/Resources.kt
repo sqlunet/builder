9e87182e844f46eec5ceab68f803b460e34990bf
1313ou <1313ou@gmail.com>
2026-09-05 17:19:37 +0200

    revert null check

--- fn/src/test/java/org/semantikos/fn/Resources.kt
+++ fn/src/test/java/org/semantikos/fn/Resources.kt
@@ -115,6 +115,7 @@ class Resources {
     fun getString(key: String): String? {
         try {
             var str: String? = resourceMap[key]!!
+            if (str == null) throw MissingResourceException(key, Resources::class.java.getName(), key)
 
             // print("\n>[$key] $str")
             str = expandPercent(str)
